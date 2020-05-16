9ad535f1b051e07058a114f3cc66b020f0a5d572
mahima5srivastava <mahima5srivastava@gmail.com>
2020-05-16 19:36:15 +0530

    Solution for Divisible Sum Pair

---
+++ Swift/divisbleSumPair.swift
@@ -0,0 +1,50 @@
+/*
+You are given an array of  integers, , and a positive integer, . Find and print the number of  pairs where  and  +  is divisible by .
+
+For example,  and . Our three pairs meeting the criteria are  and .
+
+Function Description
+
+Complete the divisibleSumPairs function in the editor below. It should return the integer count of pairs meeting the criteria.
+
+divisibleSumPairs has the following parameter(s):
+
+n: the integer length of array 
+ar: an array of integers
+k: the integer to divide the pair sum by
+Input Format
+
+The first line contains  space-separated integers,  and .
+The second line contains  space-separated integers describing the values of .
+
+Constraints
+
+Output Format
+
+Print the number of  pairs where  and  +  is evenly divisible by .
+
+Sample Input
+
+6 3
+1 3 2 6 1 2
+Sample Output
+
+ 5
+Explanation
+
+Here are the  valid pairs when :
+
+*/
+func divisibleSumPairs(n: Int, k: Int, ar: [Int]) -> Int {
+
+var pairCount = 0
+for i in 0..<ar.count {
+    for j in (i+1)..<ar.count {
+        let sum = ar[i] + ar[j]
+        if sum % k == 0 {
+            pairCount += 1
+        }
+    }
+}
+return pairCount
+}
